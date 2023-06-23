e052de217bc792fd412027d2f09b9428fb58159e
goldi2401 <goldighanghoriya30@gmail.com>
2023-06-24 00:04:38 +0530

    first commit

---
+++ backend/data/notes.js
@@ -0,0 +1,30 @@
+const notes = [
+  {
+    _id: "1",
+    title: " 1 no ",
+    content:
+      "lorem",
+    category: "cc",
+  },
+  {
+    _id: "2",
+    title: "2 no",
+    content: " lorem",
+    category: "ll",
+  },
+  {
+    _id: "3",
+    title: "  3no",
+    content: " lorem",
+    category: "Et",
+  },
+  {
+    _id: 4,
+    title: "4 no",
+    content:
+      "lorem",
+    category: "gg",
+  },
+];
+
+module.exports = notes;
